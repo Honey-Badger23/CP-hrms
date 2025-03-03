916f9300efb36f6fd62e29e9f00c185187751e87
Honey-Badger23 <michaeldavisgp29@gmail.com>
2025-03-03 15:22:20 +0200

    Initial commit

---
+++ frontend/src/components/icons/FrappeHRLogoType.vue
@@ -0,0 +1,55 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="525"
+		height="150"
+		viewBox="0"
+		fill="none"
+	>
+		<!--Background -->
+		<rect width="72" height="55" fill=#b4fae5 />
+
+		<!-- Green "C" -->
+		<text 
+			x="0" 
+			y="50" 
+			font-size="60" 
+			font-family="Arial, sans-serif" 
+			fill="#04d47c" 
+			font-weight="bold">
+			C
+		</text>
+
+		<!-- Black "P" -->
+		<text 
+			x="36" 
+			y="50" 
+			font-size="60" 
+			font-family="Arial, sans-serif" 
+			fill="#040404" 
+			font-weight="bold">
+			P
+		</text>
+
+    <!-- CommunityPal -->
+    <text
+      		x="70" 
+			y="50" 
+			font-size="60" 
+			font-family="Arial, sans-serif" 
+			fill="#04d47c" 
+			font-weight="bold">
+			Community
+    </text>
+
+    <text
+      		x="395" 
+			y="50" 
+			font-size="60" 
+			font-family="Arial, sans-serif" 
+			fill="#040404" 
+			font-weight="bold">
+			Pal
+    </text>
+	</svg>
+</template>
---
+++ roster/src/icons/FrappeHRLogo.vue
@@ -0,0 +1,34 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="117"
+		height="117"
+		viewBox="0 0 117 117"
+		fill="none"
+	>
+		<!-- White Background -->
+		<rect width="117" height="117" fill="white" />
+
+		<!-- Green "C" -->
+		<text 
+			x="30" 
+			y="75" 
+			font-size="60" 
+			font-family="Arial, sans-serif" 
+			fill="#04d47c" 
+			font-weight="bold">
+			C
+		</text>
+
+		<!-- Black "P" -->
+		<text 
+			x="65" 
+			y="75" 
+			font-size="60" 
+			font-family="Arial, sans-serif" 
+			fill="#040404" 
+			font-weight="bold">
+			P
+		</text>
+	</svg>
+</template>
